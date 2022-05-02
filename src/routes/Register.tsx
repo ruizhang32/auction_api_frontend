958c473ace91d387549f9c5fc48ec78721bc6164
rzh65 <rzh65@uclive.ac.nz>
2022-05-03 11:23:27 +1200

    Add routes and components.

---
+++ src/routes/Register.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AppBar from "../templates/AppBar";
+import SignIn from "../templates/SignIn";
+
+const Register = () => {
+  return (
+    <React.Fragment>
+      <AppBar></AppBar>
+      <SignIn></SignIn>
+    </React.Fragment>
+  );
+};
+
+export default Register;
