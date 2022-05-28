e65d4d8c2c4ac0ea61609e04edff36cb2edd3019
Rui <yoyo32nz@gmail.com>
2022-05-29 11:58:01 +1200

    change default image

--- src/Utility/util.tsx
+++ src/Utility/util.tsx
@@ -10,4 +10,4 @@ export default function equals(a:any, b:any): boolean {
   return keys.every(k => equals(a[k], b[k]));
 }
 
-export const defaultImageUrl : string = "https://overlay.imageonline.co/image.jpg";
+export const defaultImageUrl : string = "http://www.aaru.edu.jo/websites/aaru2/wp-content/plugins/learnpress/assets/images/no-image.png?Mobile=1&Source=%2F%5Flayouts%2Fmobile%2Fdispform%2Easpx%3FList%3D78b536db%252De7c7%252D45d9%252Da661%252Ddb2a2aa2fbaf%26View%3D6efc759a%252D0646%252D433c%252Dab6e%252D2f027ffe0799%26RootFolder%3D%252Fwebsites%252Faaru2%252Fwp%252Dcontent%252Fplugins%252Flearnpress%252Fassets%252Fimages%26ID%3D4786%26CurrentPage%3D1";
