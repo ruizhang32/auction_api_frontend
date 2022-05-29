06340592752aacf8a0094cd2ae9d9be930995ae7
rzh65 <rzh65@uclive.ac.nz>
2022-05-29 14:34:17 +1200

    update sign up components: add upload photo and alert components

--- src/templates/Register.tsx
+++ src/templates/Register.tsx
@@ -1,3 +1,6 @@
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
 import * as React from "react";
 import axios from "axios";
 import Avatar from "@mui/material/Avatar";
@@ -14,6 +17,22 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Navigate } from "react-router-dom";
+import {
+  Alert,
+  AlertTitle,
+  IconButton,
+  InputAdornment,
+  OutlinedInput,
+  Paper,
+} from "@mui/material";
+
+interface State {
+  amount: string;
+  password: string;
+  weight: string;
+  weightRange: string;
+  showPassword: boolean;
+}
 
 function Copyright(props: any) {
   return (
@@ -39,6 +58,13 @@ export default function Register() {
   const [isSignedUp, setIsSignedUp] = React.useState(false);
   const [errorFlag, setErrorFlag] = React.useState(false);
   const [errorMessage, setErrorMessage] = React.useState("");
+  const [values, setValues] = React.useState<State>({
+    amount: "",
+    password: "",
+    weight: "",
+    weightRange: "",
+    showPassword: false,
+  });
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -70,6 +96,24 @@ export default function Register() {
       );
   };
 
+  const handleChange =
+    (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValues({ ...values, [prop]: event.target.value });
+    };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
   if (isSignedUp) {
     return <Navigate to={"/Login"}></Navigate>;
   } else {
@@ -85,79 +129,156 @@ export default function Register() {
               alignItems: "center",
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign up
-            </Typography>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 3 }}
-            >
+            {" "}
+            <Paper sx={{ p: 5, width: 500 }}>
               <Grid container spacing={2}>
                 <Grid item xs={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="firstName"
-                    label="First Name"
-                    name="firstName"
-                    autoComplete="firstName"
-                  />
+                  <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                    <LockOutlinedIcon />
+                  </Avatar>
+                  <Typography component="h1" variant="h5">
+                    Sign up
+                  </Typography>
                 </Grid>
                 <Grid item xs={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="lastName"
-                    label="Last Name"
-                    name="lastName"
-                    autoComplete="lastName"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="new-password"
-                  />
+                  <Box>
+                    <label htmlFor="contained-button-file">
+                      <input
+                        style={{
+                          display: "none",
+                        }}
+                        accept="image/*"
+                        id="contained-button-file"
+                        multiple
+                        type="file"
+                        // onChange={onImageChange}
+                      />
+                      <Button variant="outlined" component="span">
+                        <Grid container spacing={2}>
+                          <Grid item xs={12}>
+                            <Box
+                              sx={{
+                                width: 100,
+                                height: 100,
+                                backgroundColor: "grey",
+                                border: "1px dashed grey",
+                                mb: 3,
+                              }}
+                            >
+                              <img
+                                alt="auction image"
+                                src="http://localhost:4941/api/v1/auctions/1/image"
+                                style={{
+                                  maxWidth: 100,
+                                  maxHeight: 100,
+                                }}
+                              ></img>
+                            </Box>
+                          </Grid>
+                          <Grid item xs={12} sx={{ p: 0, m: 0 }}>
+                            <Typography variant="caption">
+                              Click to update image
+                            </Typography>
+                          </Grid>
+                        </Grid>
+                      </Button>
+                    </label>
+                  </Box>
                 </Grid>
               </Grid>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
+              <Box
+                component="form"
+                noValidate
+                onSubmit={handleSubmit}
+                sx={{ mt: 3 }}
               >
-                Sign Up
-              </Button>
-              <Grid container justifyContent="flex-end">
-                <Grid item>
-                  <Link href="http://localhost:3000/login" variant="body2">
-                    Already have an account? Sign in
-                  </Link>
+                <Grid container spacing={2}>
+                  <Grid item xs={6}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="firstName"
+                      label="First Name"
+                      name="firstName"
+                      autoComplete="firstName"
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="lastName"
+                      label="Last Name"
+                      name="lastName"
+                      autoComplete="lastName"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="email"
+                      label="Email Address"
+                      name="email"
+                      autoComplete="email"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <FormControl sx={{ width: "25ch" }} variant="outlined">
+                      <InputLabel htmlFor="outlined-adornment-password">
+                        Password
+                      </InputLabel>
+                      <OutlinedInput
+                        id="outlined-adornment-password"
+                        type={values.showPassword ? "text" : "password"}
+                        value={values.password}
+                        onChange={handleChange("password")}
+                        endAdornment={
+                          <InputAdornment position="end">
+                            <IconButton
+                              aria-label="toggle password visibility"
+                              onClick={handleClickShowPassword}
+                              onMouseDown={handleMouseDownPassword}
+                              edge="end"
+                            >
+                              {values.showPassword ? (
+                                <VisibilityOff />
+                              ) : (
+                                <Visibility />
+                              )}
+                            </IconButton>
+                          </InputAdornment>
+                        }
+                        label="Password"
+                      />
+                    </FormControl>
+                  </Grid>
                 </Grid>
-              </Grid>
-            </Box>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign Up
+                </Button>
+                <Grid container justifyContent="flex-end">
+                  <Grid item>
+                    <Link href="http://localhost:3000/login" variant="body2">
+                      Already have an account? Sign in
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Box>
+              <div hidden={!errorFlag}>
+                <Alert severity="error" sx={{ width: 420, mt: 1 }}>
+                  <AlertTitle>Error</AlertTitle>
+                  <strong>{errorMessage}</strong>
+                </Alert>
+              </div>
+            </Paper>
           </Box>
-          <Copyright sx={{ mt: 5 }} />
+          <Copyright sx={{ mt: 5, mb: 10 }} />
         </Container>
       </ThemeProvider>
     );
